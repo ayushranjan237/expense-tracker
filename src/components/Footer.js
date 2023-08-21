3455e981085bfa9c53aa3cf0464c7742b52f9c05
Ayush Ranjan <106298830+ayushranjan237@users.noreply.github.com>
2023-08-21 09:12:59 +0530

    initial commit

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export const Footer = () => (
+	<footer className="footer">
+        <center>
+        <p>Expense Tracker Made by Ayush!!</p>
+        </center>
+		
+
+	</footer>
+);
+
+
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Header = () => {
+  return (
+    <header className="header">
+    <h2>
+     <center> Expense Tracker </center>
+    </h2>
+    </header>
+  )
+}
